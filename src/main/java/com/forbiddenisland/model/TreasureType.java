8fadb2b20719229c833daf1b7a2ce5e13326d3b0
D1onysuss <zhaoweiye13@gmail.com>
2025-05-24 18:41:40 +0800

    Add files via upload

---
+++ src/main/java/com/forbiddenisland/model/TreasureType.java
@@ -0,0 +1,12 @@
+package com.forbiddenisland.model;
+
+/**
+ * Enum representing the different types of treasures in the game.
+ * 代表游戏中不同类型宝藏的枚举。
+ */
+public enum TreasureType {
+    THE_EARTH_STONE,    // 大地之石
+    THE_STATUE_OF_THE_WIND, // 风之雕像
+    THE_CRYSTAL_OF_FIRE,  // 烈火圣杯
+    THE_OCEANS_CHALICE    // 海洋圣物
+} 
