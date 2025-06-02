1f57c78c41fa0be9f545fe276efc751f742dc710
MengChengkai <chengkai.meng@ucdconnect.ie>
2025-06-02 11:23:24 +0800

    Update FloodCard.java
    Translate some of the Chinese into English

--- src/main/java/com/forbiddenisland/core/model/FloodCard.java
+++ src/main/java/com/forbiddenisland/core/model/FloodCard.java
@@ -2,20 +2,39 @@ package com.forbiddenisland.core.model;
 
 import com.forbiddenisland.enums.TileName;
 
+/**
+ * Represents a Flood Card in the Forbidden Island game.
+ * Each flood card is associated with a specific tile on the island.
+ */
 public class FloodCard extends Card {
     private final TileName tileName;
 
+    /**
+     * Constructs a new FloodCard for the given tile.
+     *
+     * @param tileName the name of the tile this flood card is associated with
+     */
     public FloodCard(TileName tileName) {
-        super(tileName.getDisplayName() + "洪水卡");
+        super(tileName.getDisplayName() + " Flood Card");
         this.tileName = tileName;
     }
 
+    /**
+     * Returns the tile name associated with this flood card.
+     *
+     * @return the tile name
+     */
     public TileName getTileName() {
         return tileName;
     }
 
+    /**
+     * Returns a description of what this flood card does.
+     *
+     * @return a description string
+     */
     @Override
     public String getDescription() {
-        return "淹没" + tileName.getDisplayName() + "的洪水卡";
+        return "Flood card for " + tileName.getDisplayName();
     }
-}    
+}
